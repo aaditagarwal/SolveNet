92f2dba7d0d044d2a159b81c1db6e3749d0da6ed
Aastha Jain <jainaastha806@gmail.com>
2020-11-29 15:46:05 +0530

    Changed Model

---
+++ test.py
@@ -0,0 +1,11 @@
+import cv2
+import keras
+import numpy as np
+import pandas as pd
+from skimage import io
+from skimage.util import img_as_float
+from skimage.segmentation.slic_superpixels import slic
+import predict
+
+
+predict.run("C:/Users/jaina/Downloads/SLIC-master/SLIC-master/new.jpeg")
